43cdf5916ff85c4a6957846c5f4abba9accaa243
Nguyen Hoai Nam <heaven1xx3@gmail.com>
2021-07-15 22:35:01 +0700

    FixFormulae

--- Helpers/Formulae.cs
+++ Helpers/Formulae.cs
@@ -16,7 +16,7 @@ namespace ShapesExercise.Helpers
                 XCoordinate = cosRad * (coordinatePoint.XCoordinate - centerPoint.XCoordinate)
                                     - sinRad * (coordinatePoint.YCoordinate - centerPoint.YCoordinate) + centerPoint.XCoordinate,
                 YCoordinate = sinRad * (coordinatePoint.XCoordinate - centerPoint.XCoordinate)
-                                    - cosRad * (coordinatePoint.YCoordinate - centerPoint.YCoordinate) + centerPoint.YCoordinate
+                                    + cosRad * (coordinatePoint.YCoordinate - centerPoint.YCoordinate) + centerPoint.YCoordinate
             };
         }
     }
